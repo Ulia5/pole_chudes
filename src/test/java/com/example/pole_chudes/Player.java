dd0a97c96be286ef5973d4f045bdd1e1c6036ce8
Yulia <YG.YG.flower@gmail.com>
2024-04-08 21:22:28 +0700

    Creating a method for adding points

--- src/test/java/com/example/pole_chudes/Player.java
+++ src/test/java/com/example/pole_chudes/Player.java
@@ -2,12 +2,22 @@ package com.example.pole_chudes;
 
 public class Player {
     private String name;
+    private int points;
 
     public Player(String name) {
         this.name = name;
+        this.points = 0;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void addPoints(int newPoints) {
+        points += newPoints;
     }
 }
